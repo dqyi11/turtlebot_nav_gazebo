7f71cdd4c2239a2ec4239733f8e09becf1f3a316
walter <daqing.yi@byu.edu>
2016-02-29 09:14:42 -0700

    new files

---
+++ src/path_follower.cpp
@@ -0,0 +1,19 @@
+#include "path_follower.h"
+ 
+using namespace std;
+ 
+PathFollower::PathFollower() {
+
+}
+
+PathFollower::~PathFollower() {
+
+}
+  
+void PathFollower::load_file( string filename ) {
+
+}
+
+void PathFollower::spin() {
+
+}
